87d8562d49a4d79690946b6b4ff5630fea9d1cf1
victorsung <victorsung.class@gmail.com>
2022-01-02 19:55:58 +0900

    feat: first commit

--- src/main/java/me/victor/basic_test/v0/OrderRepositoryV0.java
+++ src/main/java/me/victor/basic_test/v0/OrderRepositoryV0.java
@@ -1,6 +1,32 @@
-package me.victor.basic_test.v0;/**
+package me.victor.basic_test.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+/**
  * Created by victorsung.
  * Date: 2022/01/02
  * Time: 7:51 PM
- */public class OrderRepositoryV0 {
+ */
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        // 저장로직
+        if (itemId.equals("ex")) {
+            throw new IllegalArgumentException("예외 발생!");
+        }
+
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
