591c3bdce005d868f36ec5170ef44b2eb00b3729
junmosung <victorsung.js@gmail.com>
2022-01-19 11:39:11 +0900

    feat: first commit

---
+++ src/test/java/me/victor/basic_test/BasicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.victor.basic_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
